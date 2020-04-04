2df54c5bbaa2f879a92169545c7c61b1ef10969d
Michael Zemler <michael.zemler@hotmail.com>
2020-04-04 21:48:10 +0200

    reworked print statements and added css reader

--- compare.py
+++ compare.py
@@ -1,7 +1,12 @@
-#find all html files
 from pathlib import Path
-result = list(Path(".").rglob("*.[hH][tT][mM][lL]"))
-print(result)
+#find all html files
+htmlFiles = list(Path(".").rglob("*.[hH][tT][mM][lL]"))
+print("Found",len(htmlFiles), "HTML file(s).")
+
+#find all css fiels
+cssFiles = list(Path(".").rglob("*.[cC][sS][sS]"))
+print("Found",len(cssFiles),"CSS File(s).")
+
 classCounter = 0
 classCounterForDiv = 0
 from html.parser import HTMLParser
@@ -9,9 +14,8 @@ from html.parser import HTMLParser
 classes = set()
 allLines = []
 #read all html files in the project folder
-for entry in result:
+for entry in htmlFiles:
     file = open (entry,"r")
-    print(file)
     lines =file.readlines()
     for line in lines:
         allLines.append(line)
@@ -23,14 +27,13 @@ class MyHTMLParser(HTMLParser):
 	
         classCounterForDiv = 0
         if tag == "div":
-            print("found new div")
+            print("Checking next div.")
             #go through all attributes
             for attr in attrs:
                 #find the class attribute
                 if attr[0] == "class":
-                    print ("classes of div",attr[1])
                     classArray = attr[1].split(" ")
-
+                    print("Found", len(classArray), "class(es) inside the div.")
                     for entry in classArray:
 
                      #check if the class already exists in the set
@@ -44,7 +47,7 @@ class MyHTMLParser(HTMLParser):
                             print("Dup class", entry)
     def handle_endtag(self, tag):
         if tag == "div":
-            print ("Finished div and added", classCounterForDiv, "new classes.")
+            print ("Finished div and added", classCounterForDiv, "new class(es).")
 
 parser = MyHTMLParser()
 for line in allLines:
@@ -54,5 +57,10 @@ for line in allLines:
     #ignore lines with closing div
     if not line == "</div>":
         parser.feed(line)
-#parser.feed('<div id= "button-nav" class = "light dark big small-go-up-button"></div>')
+
+#print the classes
 print("Found",classCounter,"classes.")
+iteratorCounter = 0
+for cssClass in classes:
+    print(iteratorCounter,": ", cssClass)
+    iteratorCounter += 1
